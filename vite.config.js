861e858f0902516d1973cfa71b1dbf0a289e4b7d
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-09-22 10:13:08 +0300

    Set up React testing environment
    This was done using:
    
    - Vitest framework
    - Jsdom library
    - React Testing Library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  globals: true,
+  environment: "jsdom",
+  setupFiles: "./src/components/tests/setup.js",
+});
