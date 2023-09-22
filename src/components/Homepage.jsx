44ced1c9c2403f2ed9380d0d82b2231d2bd5eec1
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-09-22 15:18:55 +0300

    Add homepage component

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="homepageImgWrapper">
+        <img src="" alt=""></img>
+        <h2>PrehISOria </h2>
+      </div>
+      <div className="homepageImgWrapper">
+        <img src="" alt=""></img>
+        <h2>Cats and a Dog</h2>
+      </div>
+      <div className="homepageImgWrapper">
+        <img src="" alt=""></img>
+        <h2>Meme Supreme</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
