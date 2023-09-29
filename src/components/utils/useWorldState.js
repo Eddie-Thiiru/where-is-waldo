3b6b2ee977defb2bb6d412878bb256535aa00851
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-09-29 09:23:40 +0300

    Add REST API

--- src/components/utils/useWorldState.js
+++ src/components/utils/useWorldState.js
@@ -40,12 +40,22 @@ const charactersData = {
   ],
 };
 
+const originalDimensions = {
+  prehisoria: { width: 1905, height: 1701 },
+  isord: {},
+  memesupreme: {},
+};
+
 const useWorldState = (world) => {
   const [time, setTime] = useState(0);
   const [target, setTarget] = useState({
-    xPosition: 0,
-    yPosition: 0,
+    xPos: 0,
+    yPos: 0,
+    clientWidth: 0,
+    clientHeight: 0,
   });
+  const [characters, setCharacters] = useState(charactersData[world]);
+  const [dbPositions, setDBPositions] = useState();
   const [modal, setModal] = useState({
     active: false,
     xPosition: 0,
@@ -53,20 +63,38 @@ const useWorldState = (world) => {
     heightExceeded: false,
     widthExceeded: false,
   });
-  const [characters, setCharacters] = useState(charactersData[world]);
-  const [feedback, setFeedBack] = useState({
-    wrongAnswer: false,
-    correctAnswer: { status: false, name: "" },
-  });
   const [marker, setMarker] = useState({
     1: { status: "hidden", xPosition: 0, yPosition: 0 },
     2: { status: "hidden", xPosition: 0, yPosition: 0 },
     3: { status: "hidden", xPosition: 0, yPosition: 0 },
   });
+  const [feedback, setFeedBack] = useState({
+    wrongAnswer: false,
+    correctAnswer: { status: false, name: "" },
+  });
   const [gameWon, setGameWon] = useState(false);
 
+  // Fetch characters' positions from database
   useEffect(() => {
-    // sets time for every 10 milliseconds
+    console.log("Called fetch effect");
+    fetch(`http://localhost:3000/${world}`, { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Server error");
+        }
+
+        return response.json();
+      })
+      .then((response) => {
+        setDBPositions(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [world]);
+
+  // sets time for every 10 milliseconds
+  useEffect(() => {
     let interval;
 
     if (gameWon === false) {
@@ -78,8 +106,8 @@ const useWorldState = (world) => {
     return () => clearInterval(interval);
   }, [gameWon]);
 
+  // Remove feedback popup after timeout
   useEffect(() => {
-    // Remove feedback popup after timeout
     const timeout = setTimeout(() => {
       setFeedBack({
         wrongAnswer: false,
@@ -104,8 +132,14 @@ const useWorldState = (world) => {
     let yExceeded = false;
     let xTargetPos = x;
     let yTargetPos = y;
-    console.log(x);
-    console.log(y);
+
+    // let rx = Math.round(originalSize.xAxis / x);
+    // let ry = Math.round(originalSize.yAxis / y);
+
+    // console.log(
+    //   Math.round((rx + 100) * x) / 100,
+    //   Math.round((ry + 100) * y) / 100
+    // );
 
     // if width of modal exceeds image edge
     if (x + 200 > width) {
@@ -133,7 +167,7 @@ const useWorldState = (world) => {
       }
     }
 
-    /* 
+    /*
       Also subtract X and Y positions by half of the Target Box's dimensions to
       ensure that the clicked position is centered within the Target Box.
     */
@@ -147,8 +181,10 @@ const useWorldState = (world) => {
     }
 
     setTarget({
-      xPosition: xTargetPos,
-      yPosition: yTargetPos,
+      xPos: xTargetPos,
+      yPos: yTargetPos,
+      clientWidth: width,
+      clientHeight: height,
     });
 
     setModal({
@@ -160,8 +196,47 @@ const useWorldState = (world) => {
     });
   };
 
+  // Get character coordinates form the database
+  const getDatabaseCoordinates = (characterName) => {
+    for (let i = 0; i < dbPositions.length; i++) {
+      const obj = dbPositions[i];
+
+      if (obj.characterName === characterName) {
+        const originalWidth = originalDimensions[world].width;
+        const originalHeight = originalDimensions[world].height;
+        const adjustedX = Math.round(
+          (obj.positionX / originalWidth) * target.clientWidth
+        );
+        const adjustedY = Math.round(
+          (obj.positionY / originalHeight) * target.clientHeight
+        );
+
+        return { positionX: adjustedX, positionY: adjustedY };
+      }
+    }
+  };
+
+  const isCorrectPosition = (dbCoordinates) => {
+    const { positionX, positionY } = dbCoordinates;
+    console.log(positionX, positionY);
+    console.log(modal.xPosition, modal.yPosition);
+
+    const rangeX = Math.abs(positionX - modal.xPosition);
+    const rangeY = Math.abs(positionY - modal.yPosition);
+
+    if (rangeX <= 25 && rangeY <= 25) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   const handlePopupClick = (e) => {
     const characterName = e.target.id;
+
+    const dbCoordinates = getDatabaseCoordinates(characterName);
+    const correctPosition = isCorrectPosition(dbCoordinates);
+    console.log(correctPosition);
 
     // ****** placeholder data replace with real data ********
     if (
@@ -176,7 +251,6 @@ const useWorldState = (world) => {
       });
 
       const posStr = e.target.className.split(" ")[1];
-      console.log;
 
       // Adds marker
       setMarker({
